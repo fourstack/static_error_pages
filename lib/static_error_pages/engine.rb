118b3233f94c3ebd834f8baa38dd7db4bd491425
Petrik <petrik@deheus.net>
2014-11-07 13:43:57 +0100

    Remove 502 error

--- lib/static_error_pages/engine.rb
+++ lib/static_error_pages/engine.rb
@@ -1,6 +1,6 @@
 module StaticErrorPages
   mattr_accessor :supported_errors
-  @@supported_errors = %w(404 422 500 502)
+  @@supported_errors = %w(404 422 500)
 
   class Engine < ::Rails::Engine
     isolate_namespace StaticErrorPages
