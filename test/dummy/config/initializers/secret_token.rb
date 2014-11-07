c3a938dba221414ca4c071685d8b068c198f8eae
Petrik <petrik@deheus.net>
2014-11-07 11:35:01 +0100

    Add tests

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '3049068a8abe1adb4a6da43cd376636aacf338d33e2543a19eb367de809c286b012441c0d832a3aff9c89c9e8672a5c4bd7d1bbb961e6bbe39307919616c1b57'
---
+++ test/dummy/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+
+  mount StaticErrorPages::Engine => "/static_error_pages"
+end
